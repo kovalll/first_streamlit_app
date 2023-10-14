9f19dcd1a86494d5b4490710435a20632c4a10e5
Kris <kkowalski86@gmail.com>
2023-10-14 21:10:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,14 +33,10 @@ try:
     streamlit.write('The user entered ', fruit_choice)
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
-    
 
-# normalize json response
-
-# write normalized json into dataframe
-streamlit.dataframe(fruityvice_normalized)
 streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
